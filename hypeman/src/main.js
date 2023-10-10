aadbe21299467121dd4ba2ff9e985d6cbe49d09f
boijersi <boijersi@arcada.fi>
2023-10-10 14:04:40 +0300

    importování komponent

--- hypeman/src/main.js
+++ hypeman/src/main.js
@@ -1,3 +1,10 @@
+import login from './components/login.vue'
+import loginButton from './components/loginButton.vue'
+import programChoices from './components/programChoices.vue'
+import programContent from './components/programContent.vue'
+import rating from './components/rating.vue'
+import timer from './components/timer.vue'
+import userTitle from './components/title.vue'
 
 let app = Vue.createApp({
 
@@ -10,6 +17,12 @@ let app = Vue.createApp({
 
 })
 
-import timer from './components/timer.vue'
+
 app.component('timer', timer)
+app.component('userTitle', userTitle)
+app.component('rating', rating)
+app.component('programContent', programContent)
+app.component('programChoices', programChoices)
+app.component('loginButton', loginButton)
+app.component('login', login)
 app.mount('#app')
