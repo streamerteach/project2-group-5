3c7996eb8cd930abbda8f6c81aa0bec6fc2c446f
boijersi <boijersi@arcada.fi>
2023-10-10 14:10:36 +0300

    NU FUNKAR DET <3

--- hypeman/src/main.js
+++ hypeman/src/main.js
@@ -1,10 +1,4 @@
-import login from './components/login.vue'
-import loginButton from './components/loginButton.vue'
-import programChoices from './components/programChoices.vue'
-import programContent from './components/programContent.vue'
-import rating from './components/rating.vue'
-import timer from './components/timer.vue'
-import userTitle from './components/title.vue'
+
 
 let app = Vue.createApp({
 
@@ -16,7 +10,13 @@ let app = Vue.createApp({
     }
 
 })
-
+import login from './components/login.vue'
+import loginButton from './components/loginButton.vue'
+import programChoices from './components/programChoices.vue'
+import programContent from './components/programContent.vue'
+import rating from './components/rating.vue'
+import timer from './components/timer.vue'
+import userTitle from './components/title.vue'
 
 app.component('timer', timer)
 app.component('userTitle', userTitle)
