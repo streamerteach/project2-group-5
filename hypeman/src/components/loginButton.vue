51eadbb8ded1aa87ca7787d1496b12560ca62143
boijersi <boijersi@arcada.fi>
2023-10-12 00:39:24 +0300

    Login page typ funkar

--- hypeman/src/components/loginButton.vue
+++ hypeman/src/components/loginButton.vue
@@ -1,3 +1,40 @@
 <template>
-    <h1>Hello</h1>
+  <button @click="usernameTyped" class="loginButton">SIGN IN</button>
 </template>
+
+<script>
+export default {
+  methods: {
+    //Kollar om användaren matat in användarnamn via localstorage
+    usernameTyped: function () {
+      let username = localStorage.getItem("username");
+
+      if (username === null || username === "") {
+        alert("You need to enter a username first!");
+      } else window.location.href = "../../public/programChoices.html";
+    },
+  },
+};
+</script>
+
+<style>
+.loginButton {
+    transition: 0.5s;
+    margin-top: 4rem;
+    width: 12rem;
+    height: 4rem;
+    border-radius: 15px;
+    border-style: none;
+    background-color: #5CC4FF;
+    color: white;
+    font-size: 40px;
+   
+}
+
+.loginButton:hover {
+    background-color: #4ca2d4;
+    cursor: pointer;
+    width: 14rem;
+
+}
+</style>
