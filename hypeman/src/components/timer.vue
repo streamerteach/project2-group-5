cd263db739aef680065cac3d5d5bf4de4a183351
Felix Fellman <fellefelixa@gmail.com>
2023-10-12 16:55:41 +0300

    timer in progb

--- hypeman/src/components/timer.vue
+++ hypeman/src/components/timer.vue
@@ -1,3 +1,51 @@
 <template>
-    <h1>Hello</h1>
+    
+    <div class="timer-container">
+      <button @click="startTimer(1)">Start 1-Hour Timer</button>
+      <button @click="startTimer(2)">Start 2-Hour Timer</button>
+    </div>
+    <div class="time-container">
+      <p v-if="timerRunning">Timer: {{ Math.floor(currentTime / 60) }}m {{ currentTime % 60 }}s</p>
+      <p v-else>Timer Stopped</p>
+    </div>
+  
 </template>
+<script>
+export default {
+  data() {
+    return {
+      timerRunning: false,
+      timer: null,
+      startTime: 0,
+      currentTime: 0,
+    };
+  },
+  computed: {
+    formattedTime() {
+      const minutes = Math.floor(this.currentTime / 60);
+      const seconds = this.currentTime % 60;
+      return `${minutes}m ${seconds}s`;
+    },
+  },
+  methods: {
+    startTimer(hours) {
+      if (this.timerRunning) {
+        clearInterval(this.timer);
+      }
+
+      this.startTime = hours * 60 * 60;
+      this.currentTime = this.startTime;
+      this.timerRunning = true;
+
+      this.timer = setInterval(() => {
+        if (this.currentTime <= 0) {
+          clearInterval(this.timer);
+          this.timerRunning = false;
+        } else {
+          this.currentTime--;
+        }
+      }, 1000);
+    },
+  },
+};
+</script>
