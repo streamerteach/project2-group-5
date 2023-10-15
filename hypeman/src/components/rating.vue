d7a1f2f956bb20694ca1a1ce5c986c7a98237b69
tanehans <117091615+tanehans@users.noreply.github.com>
2023-10-15 14:30:08 +0300

    rating component existerar

--- hypeman/src/components/rating.vue
+++ hypeman/src/components/rating.vue
@@ -1,3 +1,46 @@
 <template>
-    <h1>rating</h1>
-</template>
+    <div>
+      <div class="rating">
+        <span
+          v-for="star in 5"
+          :key="star"
+          @click="setRating(star)"
+          :class="{ 'rated': star <= currentRating, 'unrated': star > currentRating }"
+        >
+          ★
+        </span>
+      </div>
+      <p>Your rating: {{ currentRating }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        currentRating: 0,
+      };
+    },
+    methods: {
+      setRating(rating) {
+        this.currentRating = rating;
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .rating {
+    font-size: 30px;
+    cursor: pointer;
+  }
+  
+  .rated {
+    color: orange;
+  }
+  
+  .unrated {
+    color: gray;
+  }
+  </style>
+  
