9b90ed1a0abdf5afbeed9a8f29793275b62c803c
tanehans <117091615+tanehans@users.noreply.github.com>
2023-10-18 11:39:59 +0300

    fixade stretching texten

--- hypeman/src/utils.js
+++ hypeman/src/utils.js
@@ -1,42 +1,41 @@
+const stretchingTime = 1;
+
 export function getExerciseDescription(program, difficultyRating) {
-    // if local storage is empty, set default values for difficulty and program
-    if (!difficultyRating) {
-
-        localStorage.setItem('HIITDifficultyRating', 1);
-        localStorage.setItem('CalisthenicsDifficultyRating', 1);
-        localStorage.setItem('WeightsDifficultyRating', 1);
-    }
-
-    var stretchingTime = 1;
-    if (!program || isNaN(difficultyRating)) return [];
-  
-    switch (program) {
-      case 'Stretching':
-        return [
-          `Spin head around in circles to stretch neck muscles for ${stretchingTime} minutes`,
-          `Stretch legs by crossing them and leaning forward for ${stretchingTime} minutes`,
-          `Lean forward and touch toes to stretch hamstrings for ${stretchingTime} minutes`,
-        ];
-      case 'Calisthenics':
-        return [
-          `Do ${difficultyRating*5} pushups`,
-          `Do ${difficultyRating*7} situps`,
-          `Do ${difficultyRating*10} squats`,
-        ];
-      case 'Weights':
-        return [
-          `Do ${difficultyRating*3} bench presses`,
-          `Do ${difficultyRating*5} bicep curls`,
-          `Do ${difficultyRating*6} shoulder presses`,
-        ];
-      case 'HIIT':
-        return [
-          `Do ${difficultyRating*15} burpees`,
-          `Do ${difficultyRating*10} jumping jacks`,
-          `Do ${difficultyRating*8} mountain climbers`,
-        ];
-      default:
-        return [];
-    }
+  if (program = 'Stretching') {
+    return [
+      `Spin head around in circles to stretch neck muscles for ${stretchingTime} minutes`,
+      `Stretch legs by crossing them and leaning forward for ${stretchingTime} minutes`,
+      `Lean forward and touch toes to stretch hamstrings for ${stretchingTime} minutes`,
+    ];
   }
-  
+  if (!difficultyRating) {
+    localStorage.setItem('HIITDifficultyRating', 1);
+    localStorage.setItem('CalisthenicsDifficultyRating', 1);
+    localStorage.setItem('WeightsDifficultyRating', 1);
+  }
+
+  if (!program || isNaN(difficultyRating)) return [];
+
+  switch (program) {
+    case 'Calisthenics':
+      return [
+        `Do ${difficultyRating * 5} pushups`,
+        `Do ${difficultyRating * 7} situps`,
+        `Do ${difficultyRating * 10} squats`,
+      ];
+    case 'Weights':
+      return [
+        `Do ${difficultyRating * 3} bench presses`,
+        `Do ${difficultyRating * 5} bicep curls`,
+        `Do ${difficultyRating * 6} shoulder presses`,
+      ];
+    case 'HIIT':
+      return [
+        `Do ${difficultyRating * 15} burpees`,
+        `Do ${difficultyRating * 10} jumping jacks`,
+        `Do ${difficultyRating * 8} mountain climbers`,
+      ];
+    default:
+      return [];
+  }
+}
