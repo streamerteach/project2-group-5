f60b9d331e0736d670cdcf4ad33a280cc5a8228b
Felix Fellman <fellefelixa@gmail.com>
2023-10-19 17:31:55 +0300

    fix

--- hypeman/src/main.js
+++ hypeman/src/main.js
@@ -1,33 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import login from './components/login.vue'
-import loginbutton from './components/loginButton.vue'
-import programchoices from './components/programChoices.vue'
-import programcontent from './components/programContent.vue'
-import rating from './components/rating.vue'
-import timer from './components/timer.vue'
-import usertitle from './components/userTitle.vue'
-import programPreview from './components/programPreview.vue'
-import MusicDropdown from './components/musicDropdown.vue';
-import musicPlayer from './components/musicPlayer.vue';
-import backButton from './components/backButton.vue';
 
-const app = createApp(App);
-
-// Register your components
-app.component('login', login);
-app.component('timer', timer);
-app.component('usertitle', usertitle);
-app.component('rating', rating);
-app.component('programcontent', programcontent);
-app.component('programchoices', programchoices);
-app.component('loginbutton', loginbutton);
-app.component('programpreview', programPreview);
-app.component('musicdropdown', MusicDropdown);
-app.component('musicplayer', musicPlayer);
-app.component('backbutton', backButton);
-
-app.use(router); // Use Vue Router
-app.mount('#app');
-
+createApp(App).use(router).mount('#app');
